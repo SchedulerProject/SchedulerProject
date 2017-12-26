9243758056706c5d582d86a2f57c35ab4c03f6da
YnMPROJECT <dbstpdud09@naver.com>
2017-12-27 08:08:24 +0900

    Add MainCalender

--- app/src/main/java/com/scheduler/MainActivity.java
+++ app/src/main/java/com/scheduler/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //gridView
         GridView gridView = (GridView) findViewById(R.id.gridView);
         gridView.setNumColumns(7);
         MainCalendar calendar = new MainCalendar(getApplicationContext());
