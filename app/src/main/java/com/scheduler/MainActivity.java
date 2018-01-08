48ccff0eccdac927dc83f8450c055d4c80bf511a
YnMPROJECT <dbstpdud09@naver.com>
2018-01-08 09:05:09 +0900

    add NextMonth,PreviousMonth

--- app/src/main/java/com/scheduler/MainActivity.java
+++ app/src/main/java/com/scheduler/MainActivity.java
@@ -2,22 +2,52 @@ package com.scheduler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.GridView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button next;
+    Button previous;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //gridView
-        GridView gridView = (GridView) findViewById(R.id.gridView);
+        final GridView gridView = (GridView) findViewById(R.id.gridView);
         gridView.setNumColumns(7);
-        MainCalendar calendar = new MainCalendar(getApplicationContext());
-        for(int i=1;i<=31;i++)
-            calendar.mAdapter.addItem(new CalItem(i));
+        final MainCalendar calendar = new MainCalendar(getApplicationContext());
+
+        ((TextView) findViewById(R.id.Year_tv)).setText(String.valueOf(calendar.cyear));
+        ((TextView) findViewById(R.id.Month_tv)).setText(String.valueOf(calendar.cmonth));
+
         gridView.setAdapter(calendar.mAdapter);
+
+        next = (Button) findViewById(R.id.NextMonthButton);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar.NextMonth();
+                Log.d("log","NextButton Clicked");
+                calendar.UpdateData();
+                ((TextView) findViewById(R.id.Year_tv)).setText(String.valueOf(calendar.cyear));
+                ((TextView) findViewById(R.id.Month_tv)).setText(String.valueOf(calendar.cmonth));
+            }
+        });
+        previous = (Button) findViewById(R.id.PreviousMonthButton);
+        previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar.PreviousMonth();
+                Log.d("log","PreviousButton Clicked");
+                calendar.UpdateData();
+                ((TextView) findViewById(R.id.Year_tv)).setText(String.valueOf(calendar.cyear));
+                ((TextView) findViewById(R.id.Month_tv)).setText(String.valueOf(calendar.cmonth));
+            }
+        });
     }
 }
--- app/src/main/java/com/scheduler/MainCalendar.java
+++ app/src/main/java/com/scheduler/MainCalendar.java
@@ -3,6 +3,7 @@ package com.scheduler;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +11,11 @@ import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.zip.Inflater;
 
 /**
@@ -19,17 +24,38 @@ import java.util.zip.Inflater;
 
 public class MainCalendar {
     private Context mContext;
+    public static CalAdapter mAdapter;
 
-
-    public CalAdapter mAdapter;
+    int cyear;
+    int cmonth;
+    int cday;
+    int chour;
+    Date cdate;
 
     public MainCalendar(Context context){
         mContext = context;
         mAdapter = new CalAdapter();
+
+        long now = System.currentTimeMillis();
+        cdate = new Date(now);
+
+        SimpleDateFormat sdf_y = new SimpleDateFormat("yyyy");
+        cyear = Integer.valueOf(sdf_y.format(cdate));
+
+        SimpleDateFormat sdf_m = new SimpleDateFormat("MM");
+        cmonth = Integer.valueOf(sdf_m.format(cdate));
+
+        SimpleDateFormat sdf_d = new SimpleDateFormat("dd");
+        cday = Integer.valueOf(sdf_d.format(cdate));
+
+        SimpleDateFormat sdf_h = new SimpleDateFormat("hh");
+        chour = Integer.valueOf(sdf_h.format(cdate));
+        UpdateData();
     }
 
     class CalItemView extends LinearLayout{
         private TextView CNum_tv;
+        private TextView CMemo_tv;
         public CalItemView(Context context) {
             super(context);
             init(context);
@@ -45,15 +71,22 @@ public class MainCalendar {
             inflater.inflate(R.layout.activity_dateinfo,this,true);
 
             CNum_tv = (TextView) findViewById(R.id.DateNum);
+            CMemo_tv = (TextView) findViewById(R.id.Memo);
         }
 
         public void setCNum(int cnum){
             CNum_tv.setText(String.valueOf(cnum));
         }
+        public void setCMemo(String cmemo){
+            CMemo_tv.setText(cmemo);
+        }
     }
 
     class CalAdapter extends BaseAdapter{
         ArrayList<CalItem> items = new ArrayList<CalItem>();
+        public CalAdapter(){
+            clearItems();
+        }
 
         @Override
         public int getCount() {
@@ -73,14 +106,69 @@ public class MainCalendar {
             return i;
         }
 
+        public void setItem(int i,CalItem item) {
+            items.set(i, item);
+        }
+        public void clearItems(){
+            items.clear();
+            for(int i=0;i<42;i++)
+                addItem(new CalItem(0));
+        }
+
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
+
             CalItemView itemView = new CalItemView(mContext);
             CalItem item = items.get(i);
+
+            if(item.getDay()!=0)
             itemView.setCNum(item.day);
+            itemView.setCMemo(item.memo);
+
             return itemView;
         }
     }
 
+    public void NextMonth(){
+        cmonth ++;
+        if(cmonth>12){
+            cmonth = 1;
+            cyear++;
+        }
+    }
+    public void PreviousMonth(){
+        cmonth --;
+        if(cmonth<1){
+            cmonth = 12;
+            cyear--;
+        }
+    }
+    public void UpdateData(){
+        String month= String.valueOf(cmonth);
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        try {
+            if(cmonth<10){
+                month = "0"+ String.valueOf(cmonth);
+            }
+            Date d1 = sdf.parse(String.valueOf(cyear)+month+"01");
+            Log.d("log",String.valueOf(cyear)+month+"01" );
+            cal.setTime(d1);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        int dayNum = cal.get(Calendar.DAY_OF_WEEK);
+        Log.d("log","요일 값 : " + dayNum);
+
+        int i = 0;
+
+        mAdapter.clearItems();
+        for(i=0;i<dayNum;i++)
+            mAdapter.setItem(i,new CalItem(0));
+        for(i=1;i<=31;i++)
+            mAdapter.setItem(dayNum+i-2,new CalItem(i));
+
+        mAdapter.notifyDataSetChanged();
+    }
 
 }
